102b5a8d1c0fe9c5f382d5c5b33b614827438a78
481240788 <481240788@qq.com>
2026-08-18 16:06:33 +0800

    更新

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Agent import Agent
 async def main():
     agent = Agent()
 
-    result = await agent.run("成都今天的天气咋样啊，帮我做一个旅游计划放进我的电脑里面，存放的位置放在当前的目录下。根据今天的日期和天气一下合适的景点啊")
+    result = await agent.run("成都今天的天气咋样啊，帮我做一个旅游计划的makedown文档放进我的电脑里面。根据今天的日期和天气一下合适的景点啊")
 
     print(result)
 
